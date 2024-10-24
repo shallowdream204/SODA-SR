33027140c22622c3cd47e24bf0ace259645f8b56
Yuang Ai <60562734+shallowdream204@users.noreply.github.com>
2024-10-24 16:27:06 +0800

    Update

--- train_test_scripts/main_test_sodasr.py
+++ train_test_scripts/main_test_sodasr.py
@@ -18,7 +18,7 @@ def main():
     parser.add_argument('--scale', type=int, default=1, help='scale factor: 1, 2, 3, 4, 8')  # 1 for dn and jpeg car
     parser.add_argument('--noise', type=int, default=15, help='noise level: 15, 25, 50')
     parser.add_argument('--jpeg', type=int, default=40, help='scale factor: 10, 20, 30, 40')
-    parser.add_argument('--training_patch_size', type=int, default=128, help='patch size used in training SwinIR. '
+    parser.add_argument('--training_patch_size', type=int, default=64, help='patch size used in training SwinIR. '
                                                                              'Just used to differentiate two different settings in Table 2 of the paper. '
                                                                              'Images are NOT tested patch by patch.')
     parser.add_argument('--large_model', action='store_true', help='use large model, only provided for real image sr')
